b57b5c70822deb30574c78c6c8fed7d325e915b0
Sreeni <kattubadi426@gmail.com>
2026-07-09 08:17:20 +0530

    new code

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-# import logging
-# import os
-# from datetime import datetime
-# os.makedirs("logs", exist_ok=True)
-#
-# log_file = f"logs/log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
-#
-# logging.basicConfig(
-#     level=logging.CRITICAL,
-#     filename=log_file,
-#     format="%(asctime)s - %(levelname)s - %(message)s",
-# )
+a = 10
 
-# NDIWEC
+b = 10.0
 
+c = 'etl'
 
+d = True
 
-logging.critical("this is logging error message") # 10
-logging.debug("this is logging debug message") #30
-logging.info("this is logging logg message") #400
-logging.warning("this is logging warn message") #40
-logging.error("this is logging error message") #20
+print("details of a", a, type(a))
+print("details of b", b, type(b))
+print("details of c", c, type(c))
+print("details of c", d, type(d))
